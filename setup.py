e6c852428cecdb0811ec4c69b2dd9dc418989f5c
guest <guest@pivo.edw.ro>
2010-09-15 19:28:55 +0300

    initial commit - empty project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='PyGallery',
+      version='0.1dev',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ],
+)
