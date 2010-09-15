12ec01bcbe3bf43fa3d367c12333b1e1c965a764
guest <guest@pivo.edw.ro>
2010-09-15 19:55:44 +0300

    Read folder contents

--- pygallery.py
+++ pygallery.py
@@ -1,2 +1,10 @@
-def main():
-    print "Hello World!"
+import sys
+import os
+
+def main(params=sys.argv):
+    return params
+
+
+def read_folder(path):
+    return os.listdir(path)
+    
---
+++ tests/test_pygallery.py
@@ -0,0 +1,24 @@
+import unittest
+import pygallery
+import os
+
+
+class TestPyGallery(unittest.TestCase):
+
+    def test_cmd_arguments(self):
+        params = [1,2,3]
+        result = pygallery.main(params)
+        self.assertEqual(result, params)
+
+    def test_read_folder_contents(self):
+        params = './pics/'
+        expected = ['pic1.jpg', 'pic2.jpg']
+        result = pygallery.read_folder(params)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
+        
+        
+        
